9312fa2051278432d79c1e0ecf531f6e7e3a123f
Rémi Bastide <Remi.Bastide@irit.fr>
2022-02-15 20:22:59 +0100

    Solution Mustache OK

--- src/main/java/monprojet/controller/CityController.java
+++ src/main/java/monprojet/controller/CityController.java
@@ -37,18 +37,48 @@ public class CityController {
 		Country france = countryDao.findById(1).orElseThrow();
 		City nouvelle = new City("Nouvelle ville", france);
 		nouvelle.setPopulation(50);
-		model.addAttribute("cities", cityDao.findAll());
 		model.addAttribute("city", nouvelle);
+		model.addAttribute("cities", cityDao.findAll());
 		model.addAttribute("countries", countryDao.findAll());
 		return DEFAULT_VIEW;
 	}
 
 	/**
-	 * Insère une nouvelle ville dans la base
-	 * @param laVille la ville à insérer, initialisée par Spring à partir des valeurs soumises dans le formulaire
+	 * Affiche la page pour modifier une ville
+	 * @param model Les infos transmises à la vue (injecté par Spring)
+	 * @param laVille La requête GET contient l'id de la ville à modifier,
+	 * Spring fait une requête SQL pour récupérer la ville par son id.
+	 * @return le nom de la vue à afficher
+	 */	
+	@GetMapping(path = "edit") //à l'URL http://localhost:8989/cities/show
+	public String editeUneVille(@RequestParam("id") City laVille, Model model) {
+		log.info("On modifie la ville {}", laVille);
+		model.addAttribute("city", laVille);
+		model.addAttribute("cities", cityDao.findAll());
+		model.addAttribute("countries", countryDao.findAll());
+		return DEFAULT_VIEW;
+	}
+
+	/**
+	 * Supprime une ville
+	 * @param model Les infos transmises à la vue (injecté par Spring)
+	 * @param id l'id de la ville à supprimer,
+	 * @return une redirection vers la page d'affichage des villes
+	 */	
+	@GetMapping(path = "delete") //à l'URL http://localhost:8989/cities/show
+	public String supprimeUneVille(Integer id, Model model) {
+		log.info("On supprime la ville n° {}", id);
+		cityDao.deleteById(id);
+		// On redirige vers la page de liste des villes
+		return "redirect:/cities/show";
+	}
+
+	/**
+	 * Enregistre une ville dans la base
+	 * @param laVille la ville à enregistrer, initialisée par Spring à partir des valeurs soumises dans le formulaire
 	 * Spring fera automatiquement une requête SQL SELECT pour récupérer le pays à partir de son id.	 
-	 * Spring fera une requête SQL INSERT pour insérer la ville dans la base
-	 * @return
+	 * Spring fera une requête SQL INSERT ou UPDATE pour enregistrer la ville dans la base
+	 * @return une redirection vers la page d'affichage des villes
 	 */
 	@PostMapping(path="save") // Requête HTTP POST à l'URL http://localhost:8989/cities/save
 	public String enregistreUneVille(City laVille) {
